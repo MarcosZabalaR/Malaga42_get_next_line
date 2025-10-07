4783ee2260b5a595ffbba4b2378feb8623aa5782
Marcos Zabala Rodriguez <mzabala-@c2r1s5.42malaga.com>
2025-10-07 19:09:02 +0200

    feat: added get_next_line

---
+++ get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mzabala- <mzabala-@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/13 18:41:47 by mzabala-          #+#    #+#             */
+/*   Updated: 2024/04/13 21:45:55 by mzabala-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strrchr(const char *s, int c);
+void	ft_strjoin(char **s1, char *s2);
+
+#endif
